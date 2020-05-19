3a7c5b6eee9b8292d33f143fe46021937855b8fb
allen <2458490107@qq.com>
2020-05-19 16:59:48 +0800

    Revert "Update main.c"
    This reverts commit 0f337252acc38ab9aa3569954f01cf59a2b80b4a.

--- USER/main.c
+++ USER/main.c
@@ -5,30 +5,30 @@ int main(void)
 	OS_ERR err;
 	CPU_SR_ALLOC();
 	
-	delay_init(168);  	//时钟初始化
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//中断分组配置
+	delay_init(168);  	//ʱ�ӳ�ʼ��
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//�жϷ�������
 	Uart_Init(USART1,DISABLE,USART_IT_RXNE,2,0);
-	LED_Init();         //LED初始化
+	LED_Init();         //LED��ʼ��
 
-	//test test test
-	OSInit(&err);		//初始化UCOSIII
-	OS_CRITICAL_ENTER();//进入临界区
-	//创建开始任务
-	OSTaskCreate((OS_TCB 	* )&CreateTaskTCB,		//任务控制块
-				 (CPU_CHAR	* )"Create task", 		//任务名字
-                 (OS_TASK_PTR )Create_task, 		//任务函数
-                 (void		* )0,					//传递给任务函数的参数
-                 (OS_PRIO	  )CREATE_TASK_PRIO,    //任务优先级
-                 (CPU_STK   * )&CREATE_TASK_STK[0],	//任务堆栈基地址
-                 (CPU_STK_SIZE)CREATE_STK_SIZE/10,	//任务堆栈深度限位
-                 (CPU_STK_SIZE)CREATE_STK_SIZE,		//任务堆栈大小
-                 (OS_MSG_QTY  )0,					//任务内部消息队列能够接收的最大消息数目,为0时禁止接收消息
-                 (OS_TICK	  )0,					//当使能时间片轮转时的时间片长度，为0时为默认长度，
-                 (void   	* )0,					//用户补充的存储区
-                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //任务选项
-                 (OS_ERR 	* )&err);				//存放该函数错误时的返回值
-	OS_CRITICAL_EXIT();	//退出临界区	 
-	OSStart(&err);  //开启UCOSIII
+	
+	OSInit(&err);		//��ʼ��UCOSIII
+	OS_CRITICAL_ENTER();//�����ٽ���
+	//������ʼ����
+	OSTaskCreate((OS_TCB 	* )&CreateTaskTCB,		//������ƿ�
+				 (CPU_CHAR	* )"Create task", 		//��������
+                 (OS_TASK_PTR )Create_task, 		//������
+                 (void		* )0,					//���ݸ��������Ĳ���
+                 (OS_PRIO	  )CREATE_TASK_PRIO,    //�������ȼ�
+                 (CPU_STK   * )&CREATE_TASK_STK[0],	//�����ջ����ַ
+                 (CPU_STK_SIZE)CREATE_STK_SIZE/10,	//�����ջ�����λ
+                 (CPU_STK_SIZE)CREATE_STK_SIZE,		//�����ջ��С
+                 (OS_MSG_QTY  )0,					//�����ڲ���Ϣ�����ܹ����յ������Ϣ��Ŀ,Ϊ0ʱ��ֹ������Ϣ
+                 (OS_TICK	  )0,					//��ʹ��ʱ��Ƭ��תʱ��ʱ��Ƭ���ȣ�Ϊ0ʱΪĬ�ϳ��ȣ�
+                 (void   	* )0,					//�û�����Ĵ洢��
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //����ѡ��
+                 (OS_ERR 	* )&err);				//��Ÿú�������ʱ�ķ���ֵ
+	OS_CRITICAL_EXIT();	//�˳��ٽ���	 
+	OSStart(&err);  //����UCOSIII
 	while(1);
 }
 
